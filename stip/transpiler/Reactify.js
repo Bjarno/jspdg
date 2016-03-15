54848a38c4bc42cc6240dc8a12714ee16f75e62e
Bjarno Oeyen <mail@bjarno.be>
2016-03-15 15:47:03 +0100

    Adds Reactify transpiler (currently inheriting everything from Nodeify)

---
+++ stip/transpiler/Reactify.js
@@ -0,0 +1,40 @@
+/****************************************************************
+ *               TRANSFORMATIONS FOR REDSTONE                   *
+ ****************************************************************/
+
+
+var Reactify = (function () {
+
+    var transformer = {};
+    
+    if (typeof module !== 'undefined' && module.exports != null) {
+        Nodeify = require('./Nodeify').Nodeify;
+    }
+
+    transformer.transformVariableDecl = Nodeify.transformVariableDecl;
+    transformer.transformAssignmentExp = Nodeify.transformAssignmentExp; // <<< TODO: Change me!
+    transformer.transformBinaryExp = Nodeify.transformBinaryExp;
+    transformer.transformFunctionExp = Nodeify.transformFunctionExp;
+    transformer.transformFunctionDecl = Nodeify.transformFunctionDecl;
+    transformer.transformCallExp = Nodeify.transformCallExp;
+    transformer.transformReturnStm = Nodeify.transformReturnStm;
+    transformer.transformIfStm = Nodeify.transformIfStm;
+    transformer.transformTryStm = Nodeify.transformTryStm;
+    transformer.transformCatchClause = Nodeify.transformCatchClause;
+    transformer.transformThrowStm =  Nodeify.transformThrowStm;
+    transformer.transformBlockStm = Nodeify.transformBlockStm;
+    transformer.transformObjectExp = Nodeify.transformObjectExp;
+    transformer.transformNewExp = Nodeify.transformNewExp;
+    transformer.transformProperty = Nodeify.transformProperty;
+    transformer.transformActualParameter = Nodeify.transformActualParameter;
+    transformer.transformFormalParameter = Nodeify.transformFormalParameter;
+    transformer.transformExitNode = Nodeify.transformExitNode;
+
+    if (typeof module !== 'undefined' && module.exports != null) {
+        exports.Reactify  = transformer;
+    }
+
+    return transformer;
+
+
+})()
